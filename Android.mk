548a9c9f77b88e52b74ddd6d772fb342e63cf44a
zhanghua <zhanghua@qti.qualcomm.com>
2014-09-03 17:54:57 +0800

    fix Android.mk

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
-LOCAL_PATH := $(call my-dir)  
-  
-include $(CLEAR_VARS)  
-LOCAL_MODULE := memtester     
-LOCAL_MODULE_TAGS := optional  
-# memtester source files  
-LOCAL_SRC_FILES := memtester.c tests.c  
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/  
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= \
+	memtester.c tests.c
+LOCAL_MODULE:= memtester
+LOCAL_MODULE_FILENAME:=memtester
+LOCAL_C_INCLUDES:=$(LOCAL_PATH)
+#LOCAL_C_INCLUDES :=
+#LOCAL_STATIC_LIBRARIES :=
+#LOCAL_SHARED_LIBRARIES :=
 include $(BUILD_EXECUTABLE)
 
